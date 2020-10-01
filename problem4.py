61a6b0f0c3ea7d33c5298ae0862e83bf59291cbf
Kenji MacMartin <47537573+TheFloatingShep@users.noreply.github.com>
2020-10-01 11:25:30 -0700

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -12,3 +12,28 @@
 # - "that is a right triangle"
 # - "that is an acute triangle"
 # - "that is an obtuse triangle"
+import math
+
+a = float(input("Enter side A\n"))
+b = float(input("Enter side B\n"))
+c = float(input("Enter side C\n"))
+if a > b and a > c:
+    C = a
+    B = b
+    A = c
+if b > a and b > c:
+    C = b
+    B = a
+    A = c
+if c > b and c > a:
+    C = c
+    B = b
+    A = a
+print(A , B , C)
+#Right triangle
+s = 100 * ((math.sqrt(A**2 + B**2))/C)
+if int(s) in range (98,102):
+    print("that is a right triangle")
+#Obtuse triangle
+
+#Acute triangle
